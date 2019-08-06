7c1d1d01fe9edf5ee8ce32a747cb38572b31daa6
Breno <brenoss.magalhaes@outlook.com>
2019-08-06 13:24:01 -0300

    atualizando a página de listagem de produtos

--- src/components/register/Product/ProductView.jsx
+++ src/components/register/Product/ProductView.jsx
@@ -1,36 +1,49 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { handleCheck, tableShow,handleChecks } from "./../../../bundles/helpers.js";
+import seta from "../../../assets/seta-para-baixo-e-para-a-direita.png";
 import "./ProductView.css";
 class ProductView extends Component {
   //eslint-disable-next-line
   constructor(props) {
     super(props);
     this.state = {
-      editable: false
+      editable: false,
+      isSelected: false,
+      selected: 0
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(bool) {
+    this.setState({
+      isSelected: bool
+    });
+    console.log(this.state.isSelected);
   }
 
   componentDidMount() {
+    handleCheck();
     const checkTheader = document.querySelector("#input-Theader");
     const checks = document.querySelectorAll("#input-header");
-    checkTheader.addEventListener("click", function(e) {
-      if (checkTheader.checked == true) {
-        checks.forEach(input => {
-          input.checked = true;
+    
+    checks.forEach(check => {
+      check.addEventListener("click", () => {
+          let bool =  handleChecks(checks)
+          this.handleClick(bool)
+          
         });
-      } else {
-        checks.forEach(input => {
-          input.checked = false;
-        });
-      }
-    });
+      });
   }
+
   render() {
     return (
       <main className="main">
         <div className="container p-3">
           <div className="row  ">
-            <h3 className="col-12 col-sm-4 col-md-4 col-lg-7 no-padding-lr">Protudos</h3>
+            <h3 className="col-12 col-sm-4 col-md-4 col-lg-7 no-padding-lr">
+              Protudos
+            </h3>
             <div className="input-group mb-3 col-12 col-sm-8 col-md-8 col-lg-5 no-padding-lr">
               <input
                 type="text"
@@ -46,7 +59,7 @@ class ProductView extends Component {
               </div>
             </div>
           </div>
-          <div className="row mb-3 d-flex justify-content-between custom-filter-row">
+          <div className="row mb-2 d-flex justify-content-between custom-filter-row">
             <ul className="nav nav-pills custom-nav-product-filter">
               <li className="nav-item ">
                 {/*  eslint-disable-next-line */}
@@ -118,7 +131,94 @@ class ProductView extends Component {
               </li>
             </ul>
           </div>
-          <div className="row mb-3">
+
+          <div className={this.state.isSelected ? "row mb-3" : " d-none"}>
+            <div className="seta">
+              <img src={seta} alt="seta" />
+            </div>
+            <div className="ml-3 mr-3 counter ">{this.state.selected}</div>
+            <a href="#" className="btn text-primary">
+              <i className="fa fa-paper-plane mr-2" />salvar no e-commerce
+            </a>
+            <a href="#" className="btn text-primary">
+              <i className="fa fa-trash mr-2" />excluir produto
+            </a>
+            <div className="dropdown">
+              <button
+                className="btn btn-default text-blue dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Mais ações
+              </button>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton"
+              >
+                {/* eslint-disable-next-line */}
+                <a
+                  className="dropdown-item a-from-nav "
+                  href="javascript:void(0)"
+                >
+                  <i className="fa fa-ticket styled-icon" />
+                  Imprimir etiquetas
+                </a>
+                {/* eslint-disable-next-line */}
+                <a
+                  className="dropdown-item a-from-nav "
+                  href="javascript:void(0)"
+                >
+                  <i className="fa fa-tags styled-icon" />
+                  Incluir tags
+                </a>
+                {/* eslint-disable-next-line */}
+                <a
+                  className="dropdown-item a-from-nav "
+                  href="javascript:void(0)"
+                >
+                  <i className="fa fa-share-alt styled-icon" />
+                  Unificar cadastros
+                </a>
+                {/* eslint-disable-next-line */}
+                <a
+                  className="dropdown-item a-from-nav"
+                  href="javascript:void(0)"
+                >
+                  <i className="fa fa-edit styled-icon" />
+                  Editar dados em lote
+                </a>
+                {/* eslint-disable-next-line */}
+                <a
+                  className="dropdown-item a-from-nav "
+                  href="javascript:void(0)"
+                >
+                  <i className="fa fa-random styled-icon" />
+                  Tornar variações
+                </a>
+                {/* eslint-disable-next-line */}
+                <a
+                  className="dropdown-item a-from-nav "
+                  href="javascript:void(0)"
+                >
+                  <i className="fa fa-exchange styled-icon" />
+                  Sincronizar mapeamento e-commerce
+                </a>
+                {/* eslint-disable-next-line */}
+                <a
+                  className="dropdown-item a-from-nav "
+                  href="javascript:void(0)"
+                >
+                  <i className="fa fa-th-large styled-icon" />
+                  Transformar em kit
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="row mb-3 ">
             <Link to="cadastros/produtos/novo" className="btn btn-primary mr-2">
               Incluir Produto
             </Link>
@@ -329,67 +429,52 @@ class ProductView extends Component {
               </tbody>
             </table>
           </div>
-          
-            <nav aria-label="Page navigation" className="paginador">
-              <ul className="pagination">
-                <li className="page-item">
 
-                  {/* eslint-disable-next-line */}
-                  <a className="page-link" href="javascript:void(0)" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                  </a>
-                </li>
-                <li className="page-item">
-                  {/* eslint-disable-next-line */}
-                  <a className="page-link" href="javascript:void(0)">
-                    1
-                  </a>
-                </li>
-                <li className="page-item">
-                  {/* eslint-disable-next-line */}
-                  <a className="page-link" href="javascript:void(0)">
-                    2
-                  </a>
-                </li>
-                <li className="page-item">
-                  {/* eslint-disable-next-line */}
-                  <a className="page-link" href="javascript:void(0)">
-                    3
-                  </a>
-                </li>
-                <li className="page-item">
-                  {/* eslint-disable-next-line */}
-                  <a className="page-link" href="javascript:void(0)" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          
+          <nav aria-label="Page navigation" className="paginador">
+            <ul className="pagination">
+              <li className="page-item">
+                {/* eslint-disable-next-line */}
+                <a
+                  className="page-link"
+                  href="javascript:void(0)"
+                  aria-label="Previous"
+                >
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              <li className="page-item">
+                {/* eslint-disable-next-line */}
+                <a className="page-link" href="javascript:void(0)">
+                  1
+                </a>
+              </li>
+              <li className="page-item">
+                {/* eslint-disable-next-line */}
+                <a className="page-link" href="javascript:void(0)">
+                  2
+                </a>
+              </li>
+              <li className="page-item">
+                {/* eslint-disable-next-line */}
+                <a className="page-link" href="javascript:void(0)">
+                  3
+                </a>
+              </li>
+              <li className="page-item">
+                {/* eslint-disable-next-line */}
+                <a
+                  className="page-link"
+                  href="javascript:void(0)"
+                  aria-label="Next"
+                >
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </main>
     );
-  }
-}
-async function tableShow(e) {
-  const infos = document.querySelectorAll(".table-row-show");
-  const el = e.target;
-  const td = el.parentNode;
-  const tr = td.parentNode;
-  const info = tr.nextSibling;
-
-  infos.forEach(i => {
-    if (i.classList.contains("table-row-show") && i != info) {
-      i.classList.remove("table-row-show");
-    } else {
-      i.classList.add("table-row-show");
-    }
-  });
-
-  if (info.classList.contains("table-row-show")) {
-    info.classList.remove("table-row-show");
-  } else {
-    info.classList.add("table-row-show");
   }
 }
 
